17058efe09fceca89d87005936146aa97dd5b0a1
tjacoby2006 <55999872+tjacoby2006@users.noreply.github.com>
2021-12-18 10:49:45 -0500

    Support _n_ number of rtsp streams

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ require('dotenv').config();
 
 const NodeMediaServer = require('node-media-server');
 
+const relayTasks = [];
+const transTasks = [];
+
+let i = 1;
+while (`RTSP_URL_${i}` in process.env && `RTSP_NAME_${i}` in process.env) {
+    relayTasks.push({
+        app: 'live',
+        mode: 'static',
+        edge: process.env[`RTSP_URL_${i}`],
+        name: process.env[`RTSP_NAME_${i}`],
+        rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
+    });
+    transTasks.push({
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+        name: process.env[`RTSP_NAME_${i}`]
+    });
+    i++;
+}
+
 const config = {
     rtmp: {
         port: 1935,
@@ -17,28 +40,11 @@ const config = {
     },
     relay: {
         ffmpeg: '/usr/bin/ffmpeg',
-        tasks: [
-            {
-                app: 'live',
-                mode: 'static',
-                edge: process.env.RTSP_URL,
-                name: process.env.RTSP_NAME,
-                rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
-            }
-        ]
+        tasks: relayTasks
     },
     trans: {
         ffmpeg: '/usr/bin/ffmpeg',
-        tasks: [
-            {
-                app: 'live',
-                hls: true,
-                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-                dash: true,
-                dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
-                name: process.env.RTSP_NAME
-            }
-        ]
+        tasks: transTasks
     }
 };
 
